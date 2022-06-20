a30de33f5882ca6bbc344ea4024950a62248091f
jmaxtunde2 <jmaxtunde2@yahoo.com>
2022-06-20 10:22:37 +0100

    New update

--- src/components/UpdateResto.vue
+++ src/components/UpdateResto.vue
@@ -40,7 +40,7 @@ export default{
              this.$router.push({name:'HomePage'})
            }
         }
-    },
+        },
 
         async mounted(){
             let user = localStorage.getItem('user-info');
